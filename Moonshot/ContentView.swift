fdd540feca257b68bf77d10610c3f2c444a6f529
iiiivaska <iiiivaska@yandex.ru>
2021-10-28 19:56:30 +0300

    ScrollView

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -7,13 +7,29 @@
 
 import SwiftUI
 
+struct CustomText: View {
+    var text: String
+    
+    var body: some View {
+        Text(text)
+    }
+    
+    init(_ text: String) {
+        print("Creating a new CustomText")
+        self.text = text
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        GeometryReader { geo in
-            Image("Example")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: geo.size.width)
+        ScrollView {
+            VStack(spacing: 10) {
+                ForEach(0..<100) {
+                    CustomText("Item \($0)")
+                        .font(.title)
+                }
+            }
+            .frame(maxWidth: .infinity)
         }
     }
 }
