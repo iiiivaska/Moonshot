8419140301616da43232b7ee7f62bea72fb12ea6
iiiivaska <iiiivaska@yandex.ru>
2021-10-28 18:48:02 +0300

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Василий Буланов on 10/28/21.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
