f1564d42a81af8e3e7c9310260b411a48cad84cc
iiiivaska <iiiivaska@yandex.ru>
2021-10-28 19:00:23 +0300

    GeometryReader with Image

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -9,8 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        GeometryReader { geo in
+            Image("Example")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: geo.size.width)
+        }
     }
 }
 
